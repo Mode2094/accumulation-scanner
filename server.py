e8e4e01714a06046599fdcf95295c137542b04db
Mode2094 <96650974+Mode2094@users.noreply.github.com>
2025-11-16 02:03:50 +0300

    Update response messages and change port number

--- server.py
+++ server.py
@@ -5,17 +5,18 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "Accumulation Scanner API Running"
+    return "Accumulation Scanner Running"
 
 @app.get("/latest")
 def latest_file():
     files = [f for f in os.listdir(".") if f.endswith(".json")]
     if not files:
-        return {"error": "No JSON files found"}, 404
-    
+        return {"error": "No JSON files found yet"}, 404
+
     latest = max(files, key=os.path.getctime)
     return send_file(latest, mimetype="application/json")
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
